37c2c4d6b435068105786f15b92574e586c855df
Scott Davie <scottdavie564@msn.com>
2020-05-08 20:40:43 +0100

    Files

---
+++ RadShadowMan/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RadShadowMan
+{
+    class Program
+    {
+        static readonly string kvMdlpattern = "[\"']model[\"']?(?<path>[\"'].+[\"'])";
+        static readonly string kvVMFpattern = "[\"']file[\"']\\s+[\"'](?<path>.+)[\"']";
+
+        static List<string> ProcessReferncedVMFs(string relativePath, string fileContents)
+        {
+            Regex rx = new Regex(kvVMFpattern, RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+            MatchCollection matches = rx.Matches(fileContents);
+            if (matches.Count == 0)
+                return null;
+
+            List<string> files = new List<string>();
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                string path = groups["path"]?.Value;
+                if (string.IsNullOrWhiteSpace(path) || files.Contains(relativePath + "/" + path + ".vmf"))
+                    continue;
+
+                files.Add(relativePath + "/" + path + ".vmf");
+            }
+
+            return files;
+        }
+
+        static void GetModels(string fileContents, ref List<string> Models)
+        {
+            Regex rx = new Regex(kvMdlpattern, RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+            MatchCollection matches = rx.Matches(fileContents);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                string path = groups["path"].Value?.Replace("\"", "")?.Replace("'", "")?.Replace("models/", "")?.Trim();
+                if (string.IsNullOrWhiteSpace(path))
+                    continue;
+
+                if (!Models.Contains(path))
+                    Models.Add(path);               
+            }
+        }
+
+        static string GetFileContents(string file)
+        {
+            try
+            {
+                return File.ReadAllText(file);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ResetColor();
+                return null;
+            }
+        }
+
+        static void ProcessFile(string file)
+        {
+            string radFolder = Path.GetDirectoryName(file);
+            string radFile = radFolder + "/" + Path.GetFileNameWithoutExtension(file) + ".rad";
+            string fileContents = GetFileContents(file);
+            if (fileContents == null)
+                return;
+
+            List<string> Files = ProcessReferncedVMFs(radFolder, fileContents);
+            Files.Add(file);
+
+            List<string> Models = new List<string>();
+            foreach(string fn in Files)
+            {
+                string contents = GetFileContents(fn);
+                if (string.IsNullOrEmpty(contents))
+                    continue;
+
+                GetModels(contents, ref Models);
+            }
+
+            if(Models.Count > 0)
+            {
+                string kv = "forcetextureshadow " + Models.Aggregate((i, j) => i + "\nforcetextureshadow " + j);
+                File.WriteAllText(radFile, kv);
+
+                Console.WriteLine($"{file} contained {Models.Count} models.");
+                Console.WriteLine($"{Files.Count} files were refenced and loaded.");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"No models were found.");
+                Console.ResetColor();
+            }
+        }
+
+        static void RecurseCheck(string path)
+        {
+            foreach (string fn in Directory.GetFiles(path))
+                if (fn.Substring(fn.Length - 3) == "vmf")
+                    ProcessFile(fn);
+
+            foreach (string dir in Directory.GetDirectories(path))
+                RecurseCheck(dir);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("No argument given. Drag files or folders on this exe. or pass file/folder paths as arguments");
+                return;
+            }
+
+            foreach (string path in args)
+            {
+                string p = path.Replace("\"", "").Trim();
+                if (p.Substring(p.Length - 3) == "vmf")
+                    ProcessFile(p);
+                else if(File.Exists(p + ".vmf"))
+                    ProcessFile(p + ".vmf");
+                else
+                    RecurseCheck(p);
+            }
+
+            Console.WriteLine("Rad files completed");
+
+#if DEBUG
+            Console.ReadLine();
+#endif
+
+        }
+    }
+}
